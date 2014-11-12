e3b40c9d2c7e1026968ed7fa5a8237173bf6d86b
ari <aanders@mit.edu>
2014-11-12 17:36:37 -0500

    made demo run and simplified it

--- experiments/mountain_car_demo/mountain_car_demo.py
+++ experiments/mountain_car_demo/mountain_car_demo.py
@@ -1,25 +1,26 @@
 from rltools.MountainCar import MountainCar
-from rltools.representation import TileCoding
+from rltools.representation import TileCodingDense, TileCoding
 from rltools.valuefn import LinearTD
 from rltools.policy import Egreedy
 from rltools.agent import TabularActionSarsa
 from rltools.valuefn import LSTDlambda
+from rltools.experiment import evaluate_trial
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from itertools import product
+import os
 
 domain = MountainCar(random_start= False, max_episode=10000)
 
 act = domain.discrete_actions
-pump_pi= lambda s: act[0] if s[1]<0 else act[2]
-pump_pi_index= lambda s: 0 if s[1]<0 else 2
 
 
 proj = TileCoding(input_indicies = [np.array([0,1])],
                  ntiles = [12],
                  ntilings=[10],
+                 hashing=None,
                  state_range = domain.state_range,
                  bias_term = True)
 class PHI(object):
@@ -40,59 +41,29 @@ agent = TabularActionSarsa(domain.discrete_actions, policy, valuefn)
 
 
 num_episodes = 500
+
 def eval(valuefn):
     val = np.empty(10000)
     x = np.linspace(domain.state_range[0][0], domain.state_range[1][0], 100)
     y = np.linspace(domain.state_range[0][1], domain.state_range[1][1], 100)
     for i,s in enumerate(product(x, y)):
         s = np.array(s)
-#         val[i] = valuefn(s, agent.policy(s))
         val[i] = max([valuefn(s,0), valuefn(s,1), valuefn(s,2)])
     X, Y = np.meshgrid(x, y)
     return X, Y, val.reshape((100,100)).T
 
 
-def getVectorField(valuefn):
-    res=20
-    U = np.empty(res**2)
-    V = np.empty(res**2)
-    C = np.empty(res**2)
-    x = np.linspace(domain.state_range[0][0], domain.state_range[1][0], res)
-    y = np.linspace(domain.state_range[0][1], domain.state_range[1][1], res)
-    for i,s in enumerate(product(x, y)):
-        s = np.array(s)
-        s_next = s.copy()
-        a = np.argmax([valuefn(s,0), valuefn(s,1), valuefn(s,2)])
-        s_next[1] += (act[a][0]*10 + np.cos(3*s[0])*-0.0025)
-        s_next[:] = np.clip(s_next, *domain.state_range)
-        s_next[0] += s_next[1]
+if not os.path.exists("mountain_car_demo_figures"):
+    os.system("mkdir mountain_car_demo_figures")
 
-        s_next[:] = np.clip(s_next, *domain.state_range)
-        if s_next[0] <= domain.state_range[0][1] and s_next[1] < 0:
-            s_next[1] = 0
-        s_next -= s
-        U[i] = s_next[0]
-        V[i] = s_next[1]
-        C[i] = a/2.0
-    X, Y = np.meshgrid(x, y)
-    return X, Y, U.reshape((res,res)).T, V.reshape((res,res)).T, C.reshape((res,res)).T
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.view_init(55,55)
+ax.plot_surface(*eval(valuefn), cmap = cm.coolwarm)
+plt.contourf(*eval(valuefn))
+plt.title('initial')
+plt.savefig('mountain_car_demo_figures/0.png')
 
-
-# val = LSTDlambda(pump_pi, domain, 0.9, None, phi, 1000, 1000, 0.6)
-# plt.imshow(eval(val), interpolation= 'none')
-# plt.show()
-
-
-# plt.ion()
-# fig = plt.figure()
-# ax = fig.gca(projection='3d')
-# ax.view_init(55,55)
-# ax.plot_surface(*eval(valuefn), cmap = cm.coolwarm)
-# plt.contourf(*eval(valuefn))
-# # plt.quiver(*getVectorField(valuefn))
-# plt.title('initial')
-# plt.savefig('D:\\mountain_car_demo\\policy\\0.png')
-# plt.pause(0.0005)
 
 k = 1
 for i in xrange(num_episodes):
@@ -103,21 +74,22 @@ for i in xrange(num_episodes):
         r_t, s_t = domain.step(agent.step(r_t, s_t))
         count += 1
     agent.step(r_t, s_t)
-# #
-#     if i % 25 == 0:
-#         plt.gca().clear()
-# #         plt.gcf().clear()
-# #         plt.
-#         plt.contourf(*eval(valuefn))
-# #         plt.quiver(*getVectorField(valuefn))
-# #         plt.colorbar()
-# #         fig.clear()
-# #         ax = fig.gca(projection='3d')
-# #         ax.view_init(55,55)
-# #         ax.plot_surface(*eval(valuefn), cmap = cm.coolwarm)
-#         plt.title('episode ' + str(i))
-# #         plt.savefig('D:\\mountain_car_demo\\policy\\' + str(k) + '.png')
-#         k +=1
-#         plt.pause(0.0005)
-    print count
+ 
+    if i % 25 == 0:
+        plt.gca().clear()
+        plt.gcf().clear()
+        plt.contourf(*eval(valuefn))
+        plt.colorbar()
+        fig.clear()
+        ax = fig.gca(projection='3d')
+        ax.view_init(55,55)
+        ax.plot_surface(*eval(valuefn), cmap = cm.coolwarm)
+        plt.title('episode ' + str(i))
+        plt.savefig('mountain_car_demo_figures/' + str(k) + '.png')
+        k +=1
+        #plt.pause(0.0005)
+        
+    result =evaluate_trial(domain, agent)
+    print "current policy yields: %s, learning episode step count: %d" \
+            % (result, count)
 
